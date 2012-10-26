b984edae33adb939f73ebdec81fcf256dd91cc0a
Jeff Bryner <jeff@jeffbryner.com>
2012-10-25 20:55:52 -0700

    Log fixes, fledgling support for \.\pmem access

--- main.cpp
+++ main.cpp
@@ -15,15 +15,35 @@
 
 using namespace std;
 bool debug=false;
+bool quiet=false;
 bool allowWrite = false;
+bool bMemory = false;
 string nbdfilename = "";
 int partitionNo=0;
 
-//typedef unsigned int UINT;
-//typedef unsigned char UCHAR;
-//typedef unsigned long ULONG;
-//typedef long double LONGDOUBLE;
+//pmem windows memory driver defines
+#define PMEM_DEVICE_NAME "pmem"
+#define PMEM_WRITE_MODE 1
+// ioctl to get memory ranges from winpmem driver.
+#define PMEM_INFO_IOCTRL CTL_CODE(0x22, 0x100, 0, 3)
+#define PMEM_CTRL_IOCTRL CTL_CODE(0x22, 0x101, 0, 3)
+#define PMEM_WRITE_ENABLE CTL_CODE(0x22, 0x102, 0, 3)
+// Available modes
+#define PMEM_MODE_IOSPACE 0
+#define PMEM_MODE_PHYSICAL 1
+#pragma pack(2)
+struct pmem_info_runs {
+	__int64 start;
+	__int64 length;
+};
 
+#pragma pack(2)
+struct pmem_info_ioctrl {
+	__int64 cr3;
+	__int64 kdbg;
+	__int32 number_of_runs;
+	struct pmem_info_runs runs[1];
+};
 
 string
 vformat (const char *fmt, va_list ap)
@@ -40,7 +60,7 @@ vformat (const char *fmt, va_list ap)
     // NB. On Windows, vsnprintf returns -1 if the string didn't fit the
     // buffer.  On Linux & OSX, it returns the length it would have needed.
 
-    if (needed <= size && needed >= 0) {
+    if ((size_t)needed <= size && needed >= 0) {
         // It fit fine the first time, we're done.
         return std::string (&buf[0]);
     } else {
@@ -77,15 +97,27 @@ void usage(char *prog)
      cout<<" -n     Partition on disk to serve (0 if not specified)"<<endl;
      cout<<" -w     Enable writing (disabled by default)"<<endl;
      cout<<" -d     Enable debug messages"<<endl;
+     cout<<" -q     Be Quiet..no messages"<<endl;
      cout<<" -h     This help text"<<endl;
 }
 
 void debugLog(string message){
-    if (debug){
-    cerr<<message<<endl;
+    if (debug && !quiet){
+    cerr<<"[*] "<<message<<endl;
     }
 }
 
+void infoLog(string message){
+    if (!quiet){
+    cerr<<"[+] "<<message<<endl;
+    }
+}
+void errorLog(string message){
+     if (!quiet){
+     cerr<<"[-] "<<message<<endl;
+     }
+}
+     
 int error_mapper(DWORD winerr)
 {
 	switch(winerr){
@@ -141,7 +173,7 @@ int READ(SOCKET sh, UCHAR *whereto, int howmuch)
 {
 	int pnt = 0;
 
-	debugLog(sformat("read: %d bytes requested\n", howmuch));
+	//debugLog(sformat("read: %d bytes requested\n", howmuch));
 
 
 	while(howmuch > 0)
@@ -151,7 +183,7 @@ int READ(SOCKET sh, UCHAR *whereto, int howmuch)
 			break;
 		if (nread == SOCKET_ERROR)
 		{
-			fprintf(stderr, "Connection dropped. Error: %d\n", WSAGetLastError());
+			errorLog(sformat("Connection dropped. Error: %lu\n", WSAGetLastError()));
 			break;
 		}
 
@@ -173,7 +205,7 @@ int WRITE(SOCKET sh, UCHAR *wherefrom, int howmuch)
 			break;
 		if (nwritten == SOCKET_ERROR)
 		{
-			fprintf(stderr, "Connection dropped. Error: %d\n", WSAGetLastError());
+			errorLog(sformat("Connection dropped. Error: %lu\n", WSAGetLastError()));
 			break;
 		}
 
@@ -220,25 +252,38 @@ DWORD WINAPI blockServe(LPVOID data){
 	LARGE_INTEGER offset, fsize;
 	const char *filename;
 	filename=nbdfilename.c_str();
+	bool bmemory=false;
 
+    //memory read structures
+    char info_buffer[4096];
+    struct pmem_info_ioctrl *info = (struct pmem_info_ioctrl *)info_buffer;    
+    int i;
 
 	// open file 'filename'
-//	fh = CreateFile(filename, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (allowWrite){
-    	fh = CreateFile(filename, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    }else{
-    	fh = CreateFile(filename, GENERIC_READ , 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    //	fh = CreateFile(filename, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if (bMemory){
+       debugLog("opening memory");
+       fh=CreateFile(filename,GENERIC_READ|GENERIC_WRITE,FILE_SHARE_READ|FILE_SHARE_WRITE,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,NULL);
+    }             
+    else if (allowWrite){
+         fh = CreateFile(filename, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    }     
+    else{
+    	 fh = CreateFile(filename, GENERIC_READ , 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     }
     
 	if (fh == INVALID_HANDLE_VALUE)
 	{
-		fprintf(stderr, "Error opening file %s: %d\n", filename, GetLastError());
+		errorLog(sformat("Error opening file %s: %lu\n", filename, GetLastError()));
 		goto error;
 	}
 
 	// find length of file or starting offset of partition
 	memset(&offset, 0x00, sizeof(offset));
 	memset(&fsize, 0x00, sizeof(fsize));
+	
+	//disk, memory or file?
 	if (strnicmp(filename, "\\\\.\\PHYSICALDRIVE", 17) == 0)	/* disk */
 	{
 		DWORD dummy2;
@@ -246,12 +291,12 @@ DWORD WINAPI blockServe(LPVOID data){
 		DRIVE_LAYOUT_INFORMATION *dli = (DRIVE_LAYOUT_INFORMATION *)dummy;
 		if (!dummy)
 		{
-			fprintf(stderr, "Out of memory!\n");
+			errorLog("Out of memory!");
 			goto error;
 		}
 		if (DeviceIoControl(fh, IOCTL_DISK_GET_DRIVE_LAYOUT, NULL, 0, (void *)dli, 4096, &dummy2, NULL) == FALSE)
 		{
-			fprintf(stderr, "Cannot obtain drive layout: %d\n", GetLastError());
+			errorLog(sformat("Cannot obtain drive layout: %lu\n", GetLastError()));
 			goto error;
 		}
 
@@ -265,12 +310,41 @@ DWORD WINAPI blockServe(LPVOID data){
 		debugLog(sformat("Length: %ld,%ld (%lx%lx)\n", fsize.HighPart, fsize.LowPart, fsize.HighPart, fsize.LowPart));
 
 	}
+	else if (bMemory){
+         DWORD size;
+         int mode = PMEM_MODE_PHYSICAL;
+         //int mode = PMEM_MODE_IOSPACE;
+        // Get the memory ranges.
+        if(!DeviceIoControl(fh, PMEM_CTRL_IOCTRL, &mode, sizeof(PMEM_MODE_PHYSICAL), NULL, 0,&size, NULL)) {
+                errorLog("Failed to set acquisition mode.");
+                goto error;
+        }else{
+            // Get the memory ranges.
+            if(!DeviceIoControl(fh, PMEM_INFO_IOCTRL, NULL, 0, info_buffer, 4096,&size, NULL)) {
+                        errorLog("Failed to get memory geometry.");
+            goto error;
+            };
+
+            //assume we start at the beginning of the first run.
+            offset.QuadPart=info->runs[0].start;
+            //fsize.QuadPart=info->runs[0].length; 
+            
+                        
+            debugLog(sformat("CR3: 0x%010llX\n %d memory ranges:", info->cr3,info->number_of_runs));
+            for(i=0; i<info->number_of_runs; i++) {
+                     debugLog(sformat("Start 0x%08llX - Length 0x%08llX", info->runs[i]));
+                     fsize.QuadPart+=info->runs[i].length; 
+            };
+             
+        }
+
+   }
 	else													/* file */
 	{
 		fsize.LowPart = GetFileSize(fh, (DWORD *)&fsize.HighPart);
 		if (fsize.LowPart == 0xFFFFFFFF)
 		{
-			fprintf(stderr, "Failed to obtain filesize info!\n");
+			errorLog("Failed to obtain filesize info!");
 			goto error;
 		}
 	}
@@ -279,7 +353,7 @@ DWORD WINAPI blockServe(LPVOID data){
 	debugLog("Negotiating...sending NBDMAGIC header");
 	if (WRITE(sockh, (unsigned char *)"NBDMAGIC", 8) != 8)
 	{
-		fprintf(stderr, "Failed to send magic string\n");
+		errorLog("Failed to send magic string");
 		goto error;
 	}
 
@@ -295,7 +369,7 @@ DWORD WINAPI blockServe(LPVOID data){
 	magic[7] = 0x53;
 	if (WRITE(sockh, magic, 8) != 8)
 	{
-		fprintf(stderr, "Failed to send 2nd magic string\n");
+		errorLog("Failed to send 2nd magic string.");
 		goto error;
 	}
 
@@ -311,7 +385,7 @@ DWORD WINAPI blockServe(LPVOID data){
 	exportsize[0] = (fsize.HighPart >> 24) & 255;
 	if (WRITE(sockh, exportsize, 8) != 8)
 	{
-		fprintf(stderr, "Failed to send filesize\n");
+		errorLog("Failed to send filesize.");
 		goto error;
 	}
 	
@@ -320,7 +394,7 @@ DWORD WINAPI blockServe(LPVOID data){
 	memset(buffer, 0x00, 128);
 	if (WRITE(sockh, buffer, 128) != 128)
 	{
-		fprintf(stderr, "Failed to send a couple of 0x00s\n");
+		errorLog("Failed to send a couple of 0x00s");
 		goto error;
 	}
 
@@ -332,7 +406,7 @@ DWORD WINAPI blockServe(LPVOID data){
 		UCHAR handle[9];
 		ULONG magic, len, type;
 		LARGE_INTEGER from;
-		LARGE_INTEGER cur_offset;
+		LARGE_INTEGER cur_offset,mem_offset;
 		int err = 0;
 
 		if (getu32(sockh, &magic) == FALSE ||	// 0x12560953
@@ -342,34 +416,46 @@ DWORD WINAPI blockServe(LPVOID data){
 			getu32(sockh, &from.LowPart)  == FALSE ||	// ...low word of offset
 			getu32(sockh, &len)   == FALSE)		// length
 		{
-			fprintf(stderr, "Failed to read from socket\n");
+			errorLog("Failed to read from socket.");
 			break;
 		}
 
 
 		handle[8] = 0x00;
-		debugLog(sformat("Magic:    %lx\n", magic));
-		debugLog(sformat("Offset:   %ld,%ld (%lx%lx)\n", from.HighPart, from.LowPart, from.HighPart, from.LowPart));
-		debugLog(sformat("Len:      %ld\n", len));
-		debugLog(sformat("Handle:   %s\n", handle));
-		debugLog(sformat("Req.type: %ld (%s)\n\n", type, type?"write":"read"));
+		debugLog(sformat("Magic:    %lx", magic));
+		debugLog(sformat("Offset:   %ld,%ld (%lx%lx)", from.HighPart, from.LowPart, from.HighPart, from.LowPart));
+		debugLog(sformat("Len:      %ld", len));
+		//debugLog(sformat("Handle:   %s\n", handle));
+		debugLog(sformat("Req.type: %ld (%s)\n", type, type?"write":"read"));
 
 
 		// verify protocol
 		if (magic != 0x25609513)
 		{
-			fprintf(stderr, "Unexpected protocol version! (got: %lx, expected: 0x25609513)\n", magic);
+			errorLog(sformat("Unexpected protocol version! (got: %lx, expected: 0x25609513)", magic));
 			break;
 		}
 
 		// calculate current offset
 		cur_offset = add_li(offset, from);
+       
+       if (bmemory){
+        //reallocate if reading memory to a valid memory run.
+            for(i=0; i<info->number_of_runs; i++) {
+                     //debugLog(sformat("Start 0x%08llX - Length 0x%08llX\n", info->runs[i]));
+                     //fsize.QuadPart+=info->runs[i].length; 
+                     if ( cur_offset.QuadPart >= info->runs[i].start ) {
+                        mem_offset.QuadPart=info->runs[i].start + from.QuadPart;
+                     }
+            }
+        }
+
 
 		// seek to 'from'
 		if (SetFilePointer(fh, cur_offset.LowPart, &cur_offset.HighPart, FILE_BEGIN) == 0xFFFFFFFF)
 		{
-			fprintf(stderr, "Error seeking in file %s to position %d,%d (%x%x): %d\n", filename,
-				cur_offset.HighPart, cur_offset.LowPart, cur_offset.HighPart, cur_offset.LowPart, GetLastError());
+			errorLog(sformat("Error seeking in file %s to position %d,%d (%x%x): %lu\n", filename,
+				cur_offset.HighPart, cur_offset.LowPart, cur_offset.HighPart, cur_offset.LowPart, GetLastError()));
 			err = error_mapper(GetLastError());
 		}
 
@@ -382,7 +468,7 @@ DWORD WINAPI blockServe(LPVOID data){
 				putu32(sockh, err) == FALSE ||
 				WRITE(sockh, handle, 8) != 8)
 			{
-				fprintf(stderr, "Failed to send errorstate through socket\n");
+				errorLog("Failed to send error state through socket.");
 				break;
 			}
 		}
@@ -401,13 +487,13 @@ DWORD WINAPI blockServe(LPVOID data){
 				if (allowWrite){
     				if (WriteFile(fh, buffer, nb, &dummy, NULL) == 0)
     				{
-    					fprintf(stderr, "Failed to write to %s: %d\n", filename, GetLastError());
+    					errorLog(sformat("Failed to write to %s: %lu\n", filename, GetLastError()));
     					err = error_mapper(GetLastError());
     					break;
     				}
     				if (dummy != nb)
     				{
-    					fprintf(stderr, "Failed to write to %s: %d (written: %d, requested to write: %d)\n", filename, GetLastError(), dummy, nb);
+    					errorLog(sformat("Failed to write to %s: %d (written: %d, requested to write: %lu)\n", filename, GetLastError(), dummy, nb));
     					break;
     				}
                 }
@@ -416,7 +502,7 @@ DWORD WINAPI blockServe(LPVOID data){
 			}
 			if (len)	// connection was closed
 			{
-				fprintf(stderr, "Connection was dropped while receiving data\n");
+				errorLog("Connection was dropped while receiving data.");
 				break;
 			}
 
@@ -425,7 +511,7 @@ DWORD WINAPI blockServe(LPVOID data){
 				putu32(sockh, err) == FALSE ||
 				WRITE(sockh, handle, 8) != 8)
 			{
-				fprintf(stderr, "Failed to send through socket\n");
+				errorLog("Failed to send through socket.");
 				break;
 			}
 		}
@@ -436,7 +522,7 @@ DWORD WINAPI blockServe(LPVOID data){
 				putu32(sockh, 0) == FALSE ||
 				WRITE(sockh, handle, 8) != 8)
 			{
-				fprintf(stderr, "Failed to send through socket\n");
+				errorLog("Failed to send through socket.");
 				break;
 			}
 
@@ -450,19 +536,19 @@ DWORD WINAPI blockServe(LPVOID data){
 				// read nb to buffer;
 				if (ReadFile(fh, buffer, nb, &dummy, NULL) == 0)
 				{
-					fprintf(stderr, "Failed to read from %s: %d\n", filename, GetLastError());
+					errorLog(sformat("Failed to read from %s: %lu\n", filename, GetLastError()));
 					break;
 				}
 				if (dummy != nb)
 				{
-					fprintf(stderr, "Failed to read from %s: %d\n", filename, GetLastError());
+					errorLog(sformat("Failed to read from %s: %lu\n", filename, GetLastError()));
 					break;
 				}
 
 				// send through socket
 				if (WRITE(sockh, buffer, nb) != nb) // connection was closed
 				{
-					fprintf(stderr, "Connection dropped while sending block\n");
+					errorLog("Connection dropped while sending block.");
 					break;
 				}
 
@@ -474,11 +560,12 @@ DWORD WINAPI blockServe(LPVOID data){
 		else if (type == 2)
 		{
             //requested close
+            infoLog("Closed socket.");
             break;
         }			
 		else
 		{
-			fprintf(stderr,"Unexpected commandtype: %d\n", type);
+			errorLog(sformat("Unexpected commandtype: %d\n", type));
 			break;
 		}
 	}
@@ -487,7 +574,7 @@ DWORD WINAPI blockServe(LPVOID data){
 error:
 	if (fh != NULL && CloseHandle(fh) == 0)
 	{
-		fprintf(stderr, "Failed to close handle: %d\n", GetLastError());
+		errorLog(sformat("Failed to close handle: %lu\n", GetLastError()));
 	}
 
 	closesocket(sockh);
@@ -512,8 +599,9 @@ int main(int argc, char *argv[])
     int port=60000;
     ifstream nbdfile;
     int iError;
+    size_t found;
     
-    while ((ch=getopt(argc,argv,"c:p:f:n:hd")) != EOF)
+    while ((ch=getopt(argc,argv,"c:p:f:n:hwdq")) != EOF)
     switch(ch)
     {
         case 'c':
@@ -521,6 +609,9 @@ int main(int argc, char *argv[])
             break;
         case 'd':
             debug=true;
+            break;
+        case 'q':
+            quiet=true;
             break;
         case 'w':
             allowWrite=true;
@@ -541,18 +632,24 @@ int main(int argc, char *argv[])
             usage(argv[0]);
             return(-1);
     }
-    
-    nbdfile.open(nbdfilename.c_str(),ifstream::in|ifstream::binary);
-    if ( nbdfile.is_open() )
-    {
-        debugLog("file opened");
-        nbdfile.close();
+    found=nbdfilename.find("pmem");
+    if (found!=string::npos){
+       bMemory=true;
+       debugLog("Opening memory...delay file open until socket init.");
     }
-    else
-    {
-        cerr<<"error opening file"<<endl;
-        return(-1);
-
+    else{
+         //warn right away if file is invalid
+        nbdfile.open(nbdfilename.c_str(),ifstream::in|ifstream::binary);
+        if ( nbdfile.is_open() )
+        {
+            debugLog("File opened, valid file");
+            nbdfile.close();
+        }
+        else
+        {
+            errorLog(sformat("Error opening file: %s",nbdfilename.c_str()));
+            return(-1);
+        }
     }
     
     //socket init.
@@ -562,13 +659,13 @@ int main(int argc, char *argv[])
 	wVersionRequested = MAKEWORD(2,2);
 	iError=WSAStartup(wVersionRequested,&wsdata);
 	if (iError != NO_ERROR || iError==1){
-        cerr<<"Error initializing winsock.dll"<<endl;
+        errorLog("Error initializing winsock.dll");
         WSACleanup();
         return(-1);
     }
 	sSock=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 	if ( sSock==INVALID_SOCKET){
-        cerr<<"Couldn't open socket..quitting."<<endl;
+        errorLog("Couldn't open socket..quitting.");
         WSACleanup();
         return(-1);
     }
@@ -585,24 +682,24 @@ int main(int argc, char *argv[])
 
     if( (setsockopt(sSock, SOL_SOCKET, SO_REUSEADDR, (char*)p_int, sizeof(int)) == -1 )||
         (setsockopt(sSock, SOL_SOCKET, SO_KEEPALIVE, (char*)p_int, sizeof(int)) == -1 ) ){
-        fprintf(stderr,"Error setting options %d\n", WSAGetLastError());
+        errorLog(sformat("Error setting options %lu\n", WSAGetLastError()));
         return(-1);
     }
 	
 	if (bind(sSock,(LPSOCKADDR) &sServer,sizeof(sServer)) ==SOCKET_ERROR){
-        cerr<<"Could not bind socket to server"<<endl;
+        errorLog("Could not bind socket to server");
         return(-1);
     }
     
     //listen and start thread to handle connections
     if (listen(sSock,20)==SOCKET_ERROR){
-        cerr<<"Error listening on socket"<<endl;
+        errorLog("Error listening on socket");
     }else{
-        debugLog("listening...");
+        debugLog("Listening...");
     }
     
     while (1){
-        debugLog("init socket loop");
+        debugLog("Init socket loop");
         SOCKET sClient;
         struct sockaddr_in  clientAddr;
         int iAddrLen;
@@ -611,19 +708,16 @@ int main(int argc, char *argv[])
 		/* accept a connection */
 		sClient = accept(sSock, (struct sockaddr *)&clientAddr, &iAddrLen);
         if (inet_ntoa(clientAddr.sin_addr)!= nbdclient){
-            debugLog("rejecting connection from unauthorized source");
+            errorLog(sformat("rejecting connection from unauthorized source: %s",inet_ntoa(clientAddr.sin_addr)));
             closesocket(sClient);
         }else if (sClient != INVALID_SOCKET)
 		{
-            string logMsg ("Connection made with: ");
-            logMsg.append(inet_ntoa(clientAddr.sin_addr));
-			debugLog(logMsg);
-            
+			infoLog(sformat("Connection made with: %s",inet_ntoa(clientAddr.sin_addr)));            
             DWORD tid;
 			HANDLE th = CreateThread(NULL, 0, blockServe, (void *)sClient, 0, &tid);			
 			
         }else{
-            cerr<<"Invalid Socket"<<endl;
+            errorLog("Invalid Socket");
         }
     
     }
